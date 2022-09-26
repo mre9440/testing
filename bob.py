7f46440cf8a9c0ea1d2f21349b6834ca097229a7
Morgan Elmera <morgan@Morgans-MacBook-Air-3.local>
2022-09-26 12:10:47 -0400

    new changes

--- bob.py
+++ bob.py
@@ -1,2 +1,16 @@
-print("hi")
-dawdaw
+import turtle
+
+def make_head(x,y,radius,fillcolor):
+
+    turtle.goto(x,y)
+    turtle.begin_fill()
+    turtle.fillcolor(fillcolor)
+    turtle.circle(radius)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.pos(x,y)
+    input()
+    
+make_head(30,20,100,"red")
+input()
+
--- testing.py
+++ testing.py
@@ -1,1 +1,14 @@
-print("hello")
+import maththings
+import math
+
+def test_something():
+    #setup
+    x = 5
+    y = 4
+    expected = x + y
+    #invoke
+    actual = 9
+    # analysis
+    assert(expected == actual)
+
+test_something()
